79685043eb225f75138bb2e7760df3cd86c2e54f
Eduds007 <eduardomilanezaraujo@gmail.com>
2024-05-22 14:29:11 -0300

    docs: add PEP 8 documentation to rk4 and euler functions

--- Ex1/euler.py
+++ Ex1/euler.py
@@ -1,64 +1,71 @@
 import numpy as np
 
-def euler_leg( h, f0):
+def euler_leg(h, f0):
+    """
+    Implementação do método de Euler para resolver um sistema de equações diferenciais 
+    descrevendo o movimento de um pêndulo duplo.
 
-  #constantes
-  m1 = 0.5
-  m2 = 1
-  l1 = 1
-  l2 = 1.5
-  g = 9.8
+    Parâmetros:
+    - h (float): tamanho do passo de integração.
+    - f0 (array): vetor contendo as condições iniciais [theta1, theta2, theta1_dot, theta2_dot].
 
-  #conversão para variveis da função
+    Retorna:
+    - f_out (array): vetor contendo os valores atualizados das variáveis no tempo t + h.
+    """
 
-  teta1 = f0[0]
-  teta2 = f0[1]
-  teta11 = f0[2]
-  teta21 = f0[3]
+    # Constantes
+    m1 = 0.5
+    m2 = 1
+    l1 = 1
+    l2 = 1.5
+    g = 9.8
 
-  
-  num_g1 = (-np.sin(teta1-teta2)*(m2*l2*(teta21**2)+m2*l1*(teta11**2)*np.cos(teta1-teta2))-g*((m1 +m2)*np.sin(teta1) - m2*(np.sin(teta2)*np.cos(teta1-teta2))))
-  den_g1 = l1*(m1+m2*((np.sin(teta1-teta2))**2))
-  g1 = num_g1/den_g1
+    # Conversão para variáveis da função
+    teta1, teta2, teta11, teta21 = f0
 
+    # Equações de movimento
+    num_g1 = (-np.sin(teta1 - teta2) * (m2 * l2 * (teta21 ** 2) + m2 * l1 * (teta11 ** 2) * np.cos(teta1 - teta2))- g * ((m1 + m2) * np.sin(teta1) - m2 * (np.sin(teta2) * np.cos(teta1 - teta2))))
+    den_g1 = l1 * (m1 + m2 * ((np.sin(teta1 - teta2)) ** 2))
+    g1 = num_g1 / den_g1
 
-  num_g2 = (np.sin(teta1-teta2)*((m1+m2)*l1*(teta11**2)+m2*l2*(teta21**2)*np.cos(teta1-teta2))+g*((m1 +m2)*(np.sin(teta1)*np.cos(teta1-teta2)-np.sin(teta2))))
-  den_g2 = l2*(m1+m2*((np.sin(teta1-teta2))**2))
-  g2 = num_g2/den_g2
+    num_g2 = (np.sin(teta1 - teta2) * ((m1 + m2) * l1 * (teta11 ** 2) + m2 * l2 * (teta21 ** 2) * np.cos(teta1 - teta2))+ g * ((m1 + m2) * (np.sin(teta1) * np.cos(teta1 - teta2) - np.sin(teta2))))
+    den_g2 = l2 * (m1 + m2 * ((np.sin(teta1 - teta2)) ** 2))
+    g2 = num_g2 / den_g2
 
+    # Vetor de saída
+    fxy = np.array([teta11, teta21, g1, g2, 0, 0])
 
-  fxy = np.array([
-    teta11,
-    teta21,
-    g1,
-    g2,
-    0,
-    0
-  ])
+    f_out = f0 + h * fxy
 
-  f0[4] = g1
-  f0[5] = g2
+    return f_out
 
-  f_out = f0 + h*fxy
+def euler_steps(duration, h, f0):
+    """
+    Realiza a integração numérica usando o método de Euler.
 
-  return f_out
+    Parâmetros:
+    - duration (float): tempo total de integração.
+    - h (float): tamanho do passo de integração.
+    - f0 (array): vetor contendo as condições iniciais [theta1, theta2, theta1_dot, theta2_dot].
 
-def euler_steps( duration, h, f0 ):
-  
-  time = 0
-  steps = int(duration/h)
+    Retorna:
+    - response (array): matriz contendo os valores de tempo e as variáveis do sistema ao longo do tempo.
+    """
 
-  response = np.zeros((steps, 7))
-  response[0,1:] = f0
-  response[0,0] = 0
+    time = 0
+    steps = int(duration / h)
 
-  for i in range(1,steps):
+    response = np.zeros((steps, 7))
+    response[0, 1:] = f0
+    response[0, 0] = 0
 
-    f0 = euler_leg(h, f0)
+    for i in range(1, steps):
+        f0 = euler_leg(h, f0)
 
-    time += 1
-    response[i,0] = time*h
-    response[i, 1:] = f0
+        time += 1
+        response[i, 0] = time * h
+        response[i, 1:] = f0
 
-  return response
+    return response
+
 
--- Ex1/rk4.py
+++ Ex1/rk4.py
@@ -1,69 +1,77 @@
 import numpy as np
 
 def rk4_leg(f0):
+    """
+    Implementação do método de Runge-Kutta de quarta ordem para resolver um sistema de equações diferenciais 
+    descrevendo o movimento de um pêndulo duplo.
 
-  #constantes
-  m1 = 0.5
-  m2 = 1
-  l1 = 1
-  l2 = 1.5
-  g = 9.8
+    Parâmetros:
+    - f0 (array): vetor contendo as condições iniciais [theta1, theta2, theta1_dot, theta2_dot].
 
-  #conversão para variveis da função
+    Retorna:
+    - fxy (array): vetor contendo as derivadas das variáveis do sistema.
+    """
 
-  teta1 = f0[0]
-  teta2 = f0[1]
-  teta11 = f0[2]
-  teta21 = f0[3]
+    # Constantes
+    m1 = 0.5
+    m2 = 1
+    l1 = 1
+    l2 = 1.5
+    g = 9.8
 
-  #variaveis para cálculo
-  num_g1 = (-np.sin(teta1-teta2)*(m2*l2*(teta21**2)+m2*l1*(teta11**2)*np.cos(teta1-teta2))-g*((m1 +m2)*np.sin(teta1) - m2*(np.sin(teta2)*np.cos(teta1-teta2))))
-  den_g1 = l1*(m1+m2*((np.sin(teta1-teta2))**2))
-  g1 = num_g1/den_g1
+    # Conversão para variáveis da função
+    teta1, teta2, teta11, teta21 = f0
 
+    # Variáveis para cálculo
+    num_g1 = (-np.sin(teta1 - teta2) * (m2 * l2 * (teta21 ** 2) + m2 * l1 * (teta11 ** 2) * np.cos(teta1 - teta2))- g * ((m1 + m2) * np.sin(teta1) - m2 * (np.sin(teta2) * np.cos(teta1 - teta2))))
+    den_g1 = l1 * (m1 + m2 * ((np.sin(teta1 - teta2)) ** 2))
+    g1 = num_g1 / den_g1
 
-  num_g2 = (np.sin(teta1-teta2)*((m1+m2)*l1*(teta11**2)+m2*l2*(teta21**2)*np.cos(teta1-teta2))+g*((m1 +m2)*(np.sin(teta1)*np.cos(teta1-teta2)-np.sin(teta2))))
-  den_g2 = l2*(m1+m2*((np.sin(teta1-teta2))**2))
-  g2 = num_g2/den_g2
+    num_g2 = (np.sin(teta1 - teta2) * ((m1 + m2) * l1 * (teta11 ** 2) + m2 * l2 * (teta21 ** 2) * np.cos(teta1 - teta2))+ g * ((m1 + m2) * (np.sin(teta1) * np.cos(teta1 - teta2) - np.sin(teta2))))
+    den_g2 = l2 * (m1 + m2 * ((np.sin(teta1 - teta2)) ** 2))
+    g2 = num_g2 / den_g2
 
+    # Vetor de saída
+    fxy = np.array([teta11, teta21, g1, g2, 0, 0])
 
-  fxy = np.array([
-    teta11,
-    teta21,
-    g1,
-    g2,
-    0,
-    0
-  ])
+    return fxy
 
-  return fxy
+def rk4_steps(duration, h, f0):
+    """
+    Realiza a integração numérica usando o método de Runge-Kutta de quarta ordem.
 
-def rk4_steps( duration, h, f0 ):
-  time = 0
-  steps = int(duration/h)
-  response = np.zeros((steps, 7))
-  response[0,1:] = f0
-  response[0,0] = 0
+    Parâmetros:
+    - duration (float): tempo total em segundos.
+    - h (float): tamanho do passo de integração.
+    - f0 (array): vetor contendo as condições iniciais [theta1, theta2, theta1_dot, theta2_dot].
 
-  for i in range(1,steps):
-      k1 = rk4_leg(f0)
-      f1 = f0 + h/2*k1
-      k2 = rk4_leg(f1)
-      f2 = f0 + h/2*k2
-      k3 = rk4_leg(f2)
-      f3 = f0 + h*k3
-      k4 = rk4_leg(f3)
+    Retorna:
+    - response (array): matriz contendo os valores de tempo e as variáveis do sistema ao longo do tempo.
+    """
 
-      f0 = f0 + h/6*(k1 + 2*k2 + 2*k3 + k4)
+    time = 0
+    steps = int(duration / h)
+    response = np.zeros((steps, 7))
+    response[0, 1:] = f0
+    response[0, 0] = 0
 
-      f0[4] = k1[2]
-      f0[5] = k1[3]
+    for i in range(1, steps):
+        k1 = rk4_leg(f0)
+        f1 = f0 + h / 2 * k1
+        k2 = rk4_leg(f1)
+        f2 = f0 + h / 2 * k2
+        k3 = rk4_leg(f2)
+        f3 = f0 + h * k3
+        k4 = rk4_leg(f3)
 
+        f0 = f0 + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
+        # Atualiza as derivadas no vetor f0
+        f0[4] = k1[2]
+        f0[5] = k1[3]
 
-      time += 1
-      response[i,0] = time*h
-      response[i, 1:] = f0
+        time += 1
+        response[i, 0] = time * h
+        response[i, 1:] = f0
 
-  return response
-
+    return response
